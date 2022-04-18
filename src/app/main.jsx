30da54bed5a9a6358b9b1e75e52ee58b8b752c8b
dpelagalli <domenico.pelagalli@next.it>
2022-04-18 11:02:53 +0200

    2 series example

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,43 +2,73 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {
   Chart,
+  ChartArea,
   ChartSeries,
   ChartSeriesItem,
 } from '@progress/kendo-react-charts';
-//import 'hammerjs';
+import 'hammerjs';
 
 const ChartContainer = () => {
-  const data = [
-    { x: '1', base: 0, y: 0 },
-    { x: '2', base: 0, y: 0 },
-    { x: '3', base: 0, y: 10 },
-    { x: '4', base: 0, y: 0 },
-    { x: '5', base: 0, y: 0 },
-    { x: '6', base: 0, y: 15 },
-    { x: '7', base: 0, y: 15 },
-    { x: '8', base: 0, y: 15 },
-    { x: '9', base: 0, y: 0 },
-    { x: '10', base: 0, y: 0},
-    { x: '11', base: 0, y: 20 },
-    { x: '12', base: 0, y: 20 },
-    { x: '13', base: 0, y: 20 },
-    { x: '14', base: 0, y: 0 },
-    { x: '15', base: 0, y: 0 },
-  ];
+
+  let data1 = [];
+  let data2 = [];
+  const numItem = 100;
+
+  Array.from(Array(numItem).keys(), (value, index) => {
+    data1.push({ x: index / 100, base: 0, y: 0, color: 'red'})
+  });
+
+  Array.from(Array(numItem).keys(), (value, index) => {
+    data2.push({ x: index / 100, base: 0, y: 0, color: 'yellow' })
+  });
+
+  data1.map((value, index) => {
+    if (index === numItem / 4) {
+      data1[index - 3].y = 2 / 4;
+      data1[index - 2].y = 2 / 3;
+      data1[index - 1].y = 2;
+      data1[index].y = 2;
+      data1[index + 1].y = 2;
+      data1[index + 2].y = 2 / 3;
+      data1[index + 3].y = 2 / 4;
+    }
+  });
+
+  data2.map((value, index) => {
+    if (index === numItem / 2) {
+      data2[index - 3].y = 2 / 4;
+      data2[index - 2].y = 2 / 3;
+      data2[index - 1].y = 2;
+      data2[index].y = 2;
+      data2[index + 1].y = 2;
+      data2[index + 2].y = 2 / 3;
+      data2[index + 3].y = 2 / 4;
+    }
+  });
 
   return (
-    <Chart>
-      <ChartSeries>
-        <ChartSeriesItem
-          type="rangeArea"
-          data={data}
-          categoryField="x"
-          fromField="base"
-          toField="y"
-        />
-      </ChartSeries>
+    <Chart >
+        <ChartSeries>
+          <ChartSeriesItem
+            type="rangeArea"
+            data={data1}
+            color={data1.color}
+            categoryField="x"
+            fromField="base"
+            toField="y"
+          />
+          <ChartSeriesItem
+            type="rangeArea"
+            data={data2}
+            color={data2.color}
+            categoryField="x"
+            fromField="base"
+            toField="y"
+          />
+        </ChartSeries>
     </Chart>
   );
+  
 };
 
 ReactDOM.render(<ChartContainer />, document.querySelector('my-app'));
